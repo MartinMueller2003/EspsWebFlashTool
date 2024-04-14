b90e62db9f4f0ba9537ae7c3ba1cc0ab85fbbe9b
Martin <MartinMueller2003@yahoo.com>
2024-04-14 16:17:02 -0400

    Updated path generation to make an os specific path rather than just a linux path

--- dist/bin/upload.py
+++ dist/bin/upload.py
@@ -4,13 +4,12 @@
 
 import sys
 import os
-import tempfile
 
 sys.argv.pop(0) # Remove executable name
 toolspath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') # CWD in UNIX format
 try:
-    sys.path.insert(0, toolspath + "/pyserial") # Add pyserial dir to search path
-    sys.path.insert(0, toolspath + "/esptool") # Add esptool dir to search path
+    sys.path.insert(0, os.path.normpath(toolspath + "/pyserial")) # Add pyserial dir to search path
+    sys.path.insert(0, os.path.normpath(toolspath + "/esptool")) # Add esptool dir to search path
     import esptool # If this fails, we can't continue and will bomb below
 except:
     sys.stderr.write("pyserial or esptool directories not found next to this upload.py tool.\n")
