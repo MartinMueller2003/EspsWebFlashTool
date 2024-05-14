ce92bcdca21faada43eb02393ad945493d7e4b0c
Martin <MartinMueller2003@yahoo.com>
2024-05-14 09:31:14 -0400

    Added Winston logger With log rotate

--- src/ESPSWebFlashtoolServer.js
+++ src/ESPSWebFlashtoolServer.js
@@ -1,5 +1,7 @@
 const  Express = require("express");
 const app = Express();
+const Winston = require ("winston");
+const { format, createLogger, transports } = require("winston");
 var https = require('https');
 const cors = require("cors");
 const path = require('path'); 
@@ -8,8 +10,8 @@ const BinImage = require('./BinImage.js');
 const PORT = 5000;
 
 var fs = require("fs");
-var bodyParser = require('body-parser')
-var logger = require('morgan');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 const ApiHdr = "/api/ESPSWFT/v1/";
 const PathToSessionData = path.join(__dirname, "../sessions");
 const PathToTools = path.join(__dirname, "../tools");
@@ -17,21 +19,49 @@ const PathToDistList = path.join(__dirname, "../dists");
 const PathToCerts = path.join(__dirname, "../certs");
 const PathToHtmlData = path.join(__dirname, "../html");
 const PathToConfigData = path.join(PathToHtmlData, "conf");
+const PathToLogfiles = path.join(__dirname, "../logs");
+const LogFileName = path.join(PathToLogfiles, "log.txt");
+
+// make sure the logging directory exists
+if (!fs.existsSync(PathToLogfiles))
+{
+    fs.mkdirSync(PathToLogfiles, 0777);
+}
+
+const logger = Winston.createLogger(
+    {
+        level: 'info',
+        format: format.combine(
+            format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+            format.printf(info => `${info.timestamp}: ${info.message}`)
+        ),
+        transports:
+        [   // use console for debugging
+            // new Winston.transports.Console(),
+            new Winston.transports.File(
+            {
+                filename: LogFileName,
+                level: 'info',
+                maxsize: 500
+            }),
+        ]
+    });
+logger.info('Log system Started');
 
 manifest.begin(PathToSessionData);
 var VersionList = [];
 BinImage.GenerateVersionList(PathToDistList, VersionList);
-console.info("GenerateVersionList - Done");
+logger.info("GenerateVersionList - Done");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({  extended: true }));
 
 // setting for logging to the local console. Not needed for production
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 // Middleware that parses the body payloads as JSON to be consumed 
 // by next set of middlewares and controllers.
-app.use( bodyParser.json() );      
+app.use( bodyParser.json() );
 app.use(Express.json());
 
 // processing for the API calls
@@ -41,25 +71,25 @@ app.use(Express.json());
  {
     var RootUrl = ApiHdr + "sessions/";
 
-    // console.info("manifest body: " + JSON.stringify(req.body));
-    // console.info("manifest hostname: " + req.hostname);
-    // console.info("manifest RootUrl: " + RootUrl);
-
+    // logger.info("manifest body: " + JSON.stringify(req.body));
+    // logger.info("manifest hostname: " + req.hostname);
+    // logger.info("manifest RootUrl: " + RootUrl);
+    logger.info("Generate manifest");
     var responseData = await manifest.GenerateImageAndManifest (PathToTools, PathToDistList, req.body, PathToSessionData, RootUrl);
     res.send( responseData );
  });
 
   // process a request to get a list of versions we can supply.
-  app.get(ApiHdr + "versions", async function (req, res) 
+  app.get(ApiHdr + "versions", async function (req, res)
   {
-      // console.info("versions: " + JSON.stringify(VersionList));
+      // logger.info("versions: " + JSON.stringify(VersionList));
       res.send( JSON.stringify(VersionList) );
   });
- 
+
  // handle a session termination request
- app.delete(ApiHdr + "sessions", async function (req, res) 
+ app.delete(ApiHdr + "sessions", async function (req, res)
  {
-    // console.info("DELETE body: " + JSON.stringify(req.body));
+    // logger.info("DELETE body: " + JSON.stringify(req.body));
     let url = req.body.manifest;
     session = url.replace(ApiHdr + "sessions/", "");
     session = session.substr(0,session.lastIndexOf("/"));
@@ -68,12 +98,12 @@ app.use(Express.json());
  });
 
 // processing path for the static files for the client UI
- app.post(ApiHdr + "firmware", async function (req, res) 
+ app.post(ApiHdr + "firmware", async function (req, res)
  {
-    // console.info("firmware body: " + JSON.stringify(req.body));
+    // logger.info("firmware body: " + JSON.stringify(req.body));
     let filepath = path.join(PathToDistList, "ESPixelStick_Firmware-" + req.body.version.name);
     filepath = path.join(filepath, "firmware/firmware.json");
-    // console.info("filepath: " + filepath);
+    logger.info("Download filepath: " + filepath);
 
     res.download( filepath );
  });
@@ -88,4 +118,4 @@ var options = {
     cert: fs.readFileSync(path.join(PathToCerts, 'cert.cert'))
   };
 https.createServer(options, app).listen(PORT);
-console.info("Server Init Done");
+logger.info("Server Init Done.");
