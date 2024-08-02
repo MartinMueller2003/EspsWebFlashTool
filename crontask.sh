702877fb059d643a7547627358cb606606c24be5
Martin <MartinMueller2003@yahoo.com>
2024-08-02 17:54:20 -0400

    Fixed a logic error determining if the app is running

--- crontask.sh
+++ crontask.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 NumTasks=($(ps -A | grep -v grep | grep npm | wc -l))
-if [ $? -eq 0 ]; then
+echo "Numtasks $NumTasks"
+if [ $NumTasks -ne 0 ]; then
   echo "Process is running."
 else
   echo "Process is not running."
