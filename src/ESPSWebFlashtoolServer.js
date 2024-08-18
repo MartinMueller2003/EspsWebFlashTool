1f23b43ae400be26584a2a29b8b2c15b54d9e37d
Martin <MartinMueller2003@yahoo.com>
2024-08-18 11:03:03 -0400

    Added build of image list prior to sending to the UI rather than on startup. Allows images to be added without restarting the server. Fixed octal syntax error

--- src/ESPSWebFlashtoolServer.js
+++ src/ESPSWebFlashtoolServer.js
@@ -25,7 +25,7 @@ const LogFileName = path.join(PathToLogfiles, "log.txt");
 // make sure the logging directory exists
 if (!fs.existsSync(PathToLogfiles))
 {
-    fs.mkdirSync(PathToLogfiles, 0777);
+    fs.mkdirSync(PathToLogfiles, 0o777);
 }
 
 const logger = Winston.createLogger(
@@ -59,16 +59,16 @@ app.use(bodyParser.urlencoded({  extended: true }));
 // setting for logging to the local console. Not needed for production
 app.use(morgan("dev"));
 
-// Middleware that parses the body payloads as JSON to be consumed 
+// Middleware that parses the body payloads as JSON to be consumed
 // by next set of middlewares and controllers.
 app.use( bodyParser.json() );
 app.use(Express.json());
 
 // processing for the API calls
 
- // process a request to create a mono image and manifest.
- app.post(ApiHdr + "manifest", async function (req, res)
- {
+// process a request to create a mono image and manifest.
+app.post(ApiHdr + "manifest", async function (req, res)
+{
     var RootUrl = ApiHdr + "sessions/";
 
     // console.info("manifest body: " + JSON.stringify(req.body));
@@ -78,40 +78,42 @@ app.use(Express.json());
     logger.info("Generate manifest");
     var responseData = await manifest.GenerateImageAndManifest (PathToTools, PathToDistList, req.body, PathToSessionData, RootUrl);
     res.send( responseData );
- });
+});
 
-  // process a request to get a list of versions we can supply.
-  app.get(ApiHdr + "versions", async function (req, res)
-  {
-      // logger.info("versions: " + JSON.stringify(VersionList));
-      res.send( JSON.stringify(VersionList) );
-  });
+// process a request to get a list of versions we can supply.
+app.get(ApiHdr + "versions", async function (req, res)
+{
+    BinImage.GenerateVersionList(PathToDistList, VersionList);
+    logger.info("GenerateVersionList - Done");
+    // logger.info("versions: " + JSON.stringify(VersionList));
+    res.send( JSON.stringify(VersionList) );
+});
 
- // handle a session termination request
- app.delete(ApiHdr + "sessions", async function (req, res)
- {
+// handle a session termination request
+app.delete(ApiHdr + "sessions", async function (req, res)
+{
     // logger.info("DELETE body: " + JSON.stringify(req.body));
     let url = req.body.manifest;
     session = url.replace(ApiHdr + "sessions/", "");
     session = session.substr(0,session.lastIndexOf("/"));
     var responseData = await manifest.DeleteImageAndManifest (session, PathToSessionData);
     res.sendStatus( responseData );
- });
+});
 
 // processing path for the static files for the client UI
- app.post(ApiHdr + "firmware", async function (req, res)
- {
+app.post(ApiHdr + "firmware", async function (req, res)
+{
     // logger.info("firmware body: " + JSON.stringify(req.body));
     let filepath = path.join(PathToDistList, "ESPixelStick_Firmware-" + req.body.version.name);
     filepath = path.join(filepath, "firmware/firmware.json");
     logger.info("Download filepath: " + filepath);
 
     res.download( filepath );
- });
+});
 
- app.use(Express.static(PathToHtmlData));
- app.use(ApiHdr + "sessions", Express.static(PathToSessionData));
- app.use(ApiHdr + "config", Express.static(path.join(PathToConfigData, "config.json")));
+app.use(Express.static(PathToHtmlData));
+app.use(ApiHdr + "sessions", Express.static(PathToSessionData));
+app.use(ApiHdr + "config", Express.static(path.join(PathToConfigData, "config.json")));
 
 // start the express server
 var options = {
