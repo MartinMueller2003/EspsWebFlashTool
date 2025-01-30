30c3d416b34d793b8dffc5ad3481f63a677509bd
Martin <MartinMueller2003@yahoo.com>
2025-01-30 14:27:15 -0500

    Renew the cert on each reboot and each poll time

--- crontask.sh
+++ crontask.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#systemctl disable apache2 && sudo systemctl stop apache2
+certbot renew
+
 NumTasks=($(ps -A | grep -v grep | grep npm | wc -l))
 echo "Numtasks $NumTasks"
 if [ $NumTasks -ne 0 ]; then
